6bbedaf36112396617faca2d0967c2eb8c3eeedc
Kadir Bozat <kadir.bozat@tail.com.tr>
2025-03-03 12:12:53 +0300

    fix

--- mainscript.py
+++ mainscript.py
@@ -272,50 +272,53 @@ with open(my_dict['json_path'],'r') as f:
     d = json.load(f)
     f.close()
     print(d['status'])
+    #if new list ready start to creating for persons
     if d['status'] == 'preparing':
 
         d['status'] = 'created'
         
+       
+    
+        for p in [{
+                    "title" : "Ahmet",
+                    "month" : "Ocak",
+                    "day"   : '12'
+                },{
+                    "title" : "Kadir",
+                    "month" : "Aralık",
+                    "day"   : '22'
+                }]:
+
+            editSvg(my_dict['title_svg'],'{title}',p['title'])
+            editSvg(my_dict['date_svg'],'{day}',p['day'])
+            editSvg(my_dict['date_svg'],'{month}',p['month'])
+
+            for sv in [my_dict['title_svg']]:
+                svg_to_gif(sv, output_dir+my_dict['title_svg']+".gif",1920,1080,80)
+                processImage(output_dir+my_dict['title_svg']+".gif")
+                images_to_mp4(output_dir+my_dict['title_svg']+'alpha.webm',60,10)
+
+            for sv in [my_dict['date_svg']]:
+                svg_to_gif(sv, output_dir+'/'+my_dict['date_svg']+".gif",1920,1080,80)
+                processImage(output_dir+'/'+my_dict['date_svg']+".gif")
+                images_to_mp4(output_dir+'/'+my_dict['date_svg']+'alpha.webm',60,10)
+
+            editSvg(my_dict['title_svg'],p['title']+'</tspan>','{title}'+'</tspan>')
+            editSvg(my_dict['date_svg'],p['day']+'</tspan>','{day}'+'</tspan>')
+            editSvg(my_dict['date_svg'],p['month']+'</tspan>','{month}'+'</tspan>')
 
 
+
+
+            vdo_with_alpha(output_dir+my_dict['title_svg']+'alpha.webm', "videos/dogumgunu-video1.mp4", "dogumgunu-video1-edited.mp4")
+            vdo_with_alpha(output_dir+my_dict['date_svg']+'alpha.webm', "videos/dogumgunu-video2.mp4", "dogumgunu-video2-edited.mp4")
+
+            createMovie("output-"+p['title']+".mp4" , my_dict['script_title'].replace("{title}",p['title']) , my_dict['script_date'].replace("{date}",p['day']+' '+p['month']))
+
+        #last 
+        #write last status to file
         with open(my_dict['json_path'], 'w') as f:
             json.dump(d, f)
-    #last 
-    '''for p in [{
-                "title" : "Ahmet",
-                "month" : "Ocak",
-                "day"   : '12'
-            },{
-                "title" : "Kadir",
-                "month" : "Aralık",
-                "day"   : '22'
-            }]:
-
-        editSvg(my_dict['title_svg'],'{title}',p['title'])
-        editSvg(my_dict['date_svg'],'{day}',p['day'])
-        editSvg(my_dict['date_svg'],'{month}',p['month'])
-
-        for sv in [my_dict['title_svg']]:
-            svg_to_gif(sv, output_dir+my_dict['title_svg']+".gif",1920,1080,80)
-            processImage(output_dir+my_dict['title_svg']+".gif")
-            images_to_mp4(output_dir+my_dict['title_svg']+'alpha.webm',60,10)
-
-        for sv in [my_dict['date_svg']]:
-            svg_to_gif(sv, output_dir+'/'+my_dict['date_svg']+".gif",1920,1080,80)
-            processImage(output_dir+'/'+my_dict['date_svg']+".gif")
-            images_to_mp4(output_dir+'/'+my_dict['date_svg']+'alpha.webm',60,10)
-
-        editSvg(my_dict['title_svg'],p['title']+'</tspan>','{title}'+'</tspan>')
-        editSvg(my_dict['date_svg'],p['day']+'</tspan>','{day}'+'</tspan>')
-        editSvg(my_dict['date_svg'],p['month']+'</tspan>','{month}'+'</tspan>')
-
-
-
-
-        vdo_with_alpha(output_dir+my_dict['title_svg']+'alpha.webm', "videos/dogumgunu-video1.mp4", "dogumgunu-video1-edited.mp4")
-        vdo_with_alpha(output_dir+my_dict['date_svg']+'alpha.webm', "videos/dogumgunu-video2.mp4", "dogumgunu-video2-edited.mp4")
-
-        createMovie("output-"+p['title']+".mp4" , my_dict['script_title'].replace("{title}",p['title']) , my_dict['script_date'].replace("{date}",p['day']+' '+p['month']))'''
 
    
 
