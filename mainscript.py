355045f9eca65cf30f0aebf078e1250c38f0c19d
Kadir Bozat <kadir.bozat@tail.com.tr>
2025-03-03 11:25:48 +0300

    test

--- mainscript.py
+++ mainscript.py
@@ -79,6 +79,7 @@ def svg_to_gif(svg_file, gif_file, width, height, duration=3000, frames=60):
     # Set up a headless Chrome browser
     chrome_options = Options()
     chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--user-data-dir")
     driver = webdriver.Chrome(options=chrome_options)
 
     # Create a simple HTML file to display the SVG
