73a20469ad9cd2dd6d8320063d8e2df2b1bd98b6
Kadir Bozat <kadir.bozat@tail.com.tr>
2025-04-30 00:38:02 +0300

    test

--- mainscript.py
+++ mainscript.py
@@ -79,11 +79,11 @@ def svg_to_gif(svg_file, gif_file, width, height, duration=3000, frames=60):
     # Set up a headless Chrome browser
     chrome_options = Options()
     chrome_options.add_argument("--headless")
-    #chrome_options.add_argument("--no-sandbox")
-    #chrome_options.add_argument('--profile-directory=Default')
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument('--profile-directory=Default')
 
    
-    #chrome_options.add_argument("--user-data-dir=/tmp/chrome-user-data")
+    chrome_options.add_argument("--user-data-dir=/tmp/chrome-user-data1")
 
     driver = webdriver.Chrome(options=chrome_options)
 
