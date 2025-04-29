6bb854805a25299f65380c71e433956856cda13c
Kadir Bozat <kadir.bozat@tail.com.tr>
2025-04-29 10:00:46 +0300

    test

--- mainscript.py
+++ mainscript.py
@@ -80,6 +80,7 @@ def svg_to_gif(svg_file, gif_file, width, height, duration=3000, frames=60):
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument('--profile-directory=Default')
 
    
     chrome_options.add_argument("--user-data-dir=/tmp/chrome-user-data")
