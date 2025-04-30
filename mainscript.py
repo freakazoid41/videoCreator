3c94799c0749d30169f74ec09113a9f39b0ec8e3
Kadir Bozat <kadir.bozat@tail.com.tr>
2025-04-30 18:03:01 +0300

    test

--- mainscript.py
+++ mainscript.py
@@ -301,8 +301,7 @@ def setLockFile(lock = True):
 with open(my_dict['json_path'],'r') as f:
     d = json.load(f)
     f.close()
-    print(os.path.exists(my_dict['json_path']+'.lock'))
-    exit()
+   
     #if new list ready start to creating for persons
     if (d['status'] == 'waiting' or d['status'] == 'preparing') and not os.path.exists(my_dict['json_path']+'.lock'):
         #set lock file 
